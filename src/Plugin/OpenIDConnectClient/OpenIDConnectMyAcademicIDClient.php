75c4ad5430d80510d1632e1cc3c56a4bb6a6ef5b
Tiago Simões <tiago.simoes@uni-foundation.eu>
2020-10-13 12:09:08 +0200

    Cloned the generic OIDC client to the myacademicid namespace

---
+++ src/Plugin/OpenIDConnectClient/OpenIDConnectMyAcademicIDClient.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Drupal\openid_connect_myacademicid\Plugin\OpenIDConnectClient;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\openid_connect\Plugin\OpenIDConnectClientBase;
+
+/**
+ * OpenID Connect client for MyAcademicID.
+ *
+ * @OpenIDConnectClient(
+ *   id = "myacademicid",
+ *   label = @Translation("MyAcademicID")
+ * )
+ */
+class OpenIDConnectMyAcademicIDClient extends OpenIDConnectClientBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+      'authorization_endpoint' => '',
+      'token_endpoint' => '',
+      'userinfo_endpoint' => '',
+    ] + parent::defaultConfiguration();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildConfigurationForm($form, $form_state);
+
+    $form['authorization_endpoint'] = [
+      '#title' => $this->t('Authorization endpoint'),
+      '#type' => 'textfield',
+      '#default_value' => $this->configuration['authorization_endpoint'],
+    ];
+    $form['token_endpoint'] = [
+      '#title' => $this->t('Token endpoint'),
+      '#type' => 'textfield',
+      '#default_value' => $this->configuration['token_endpoint'],
+    ];
+    $form['userinfo_endpoint'] = [
+      '#title' => $this->t('UserInfo endpoint'),
+      '#type' => 'textfield',
+      '#default_value' => $this->configuration['userinfo_endpoint'],
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getEndpoints() {
+    return [
+      'authorization' => $this->configuration['authorization_endpoint'],
+      'token' => $this->configuration['token_endpoint'],
+      'userinfo' => $this->configuration['userinfo_endpoint'],
+    ];
+  }
+
+}
